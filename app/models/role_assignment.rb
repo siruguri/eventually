56e8a46e1cda729e53fbfcb96bc977772bfe7e9e
Sameer S <siruguri@gmail.com>
2013-06-12 17:37:32 +0100

    removed old model files

--- app/models/role_assignment.rb
+++
@@ -1,5 +0,0 @@
-class RoleAssignment < ActiveRecord::Base
-  belongs_to :role
-  belongs_to :person
-  # attr_accessible :title, :body
-end
--- app/models/team_role.rb
+++
@@ -1,6 +0,0 @@
-class TeamRole < ActiveRecord::Base
-  attr_accessible :name
-
-  has_many :persons, through: :role_assignments
-  has_many :role_assignments
-end
